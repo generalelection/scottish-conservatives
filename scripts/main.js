73c123a60d5eda87bd336a14037412670da90f25
Sai Raghu Neelothpal Saraswatibhatla <neelu.rsa@gmail.com>
2016-05-17 18:28:29 +0100

    Add year to copyright

--- scripts/main.js
+++ scripts/main.js
@@ -8,7 +8,7 @@ var links=[
 ];
 
 function insertDisc(iden){
-  $(iden).append('<p><b>NOTE:</b> This is a website created for a school project, and has no links with the official Scottish Conservative Unionist Party. Scottish Conservatives, Scottish Conservative Unionist Party, the Tory Tree logo, the blue UK flag (used as the background image on this page), and all other trademarks belong to the Scottish Conservative Unionist Party, the UK Conservative Party, and/or their respective owners.</p><br><div class="copy">&copy; Unofficial Scottish Conservative Unionist Party</div>');
+  $(iden).append('<p><b>NOTE:</b> This is a website created for a school project, and has no links with the official Scottish Conservative Unionist Party. Scottish Conservatives, Scottish Conservative Unionist Party, the Tory Tree logo, the blue UK flag (used as the background image on this page), and all other trademarks belong to the Scottish Conservative Unionist Party, the UK Conservative Party, and/or their respective owners.</p><br><div class="copy">&copy; 2016 Unofficial Scottish Conservative Unionist Party</div>');
 }
 
 function bar() {
